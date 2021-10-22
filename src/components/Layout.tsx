934f16edd5cc38bd99d0065e2ce613bdc5f2ec99
Mahciavelli <tmakaveli643@gmail.com>
2021-10-22 01:52:27 +0100

    template ready

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,11 +4,9 @@ import Footer from './Footer'
 const Layout = (props: Props) => {
   const { children } = props
   return (
-    <div className="bg-dark-1 min-h-screen ">
+    <div>
       <Navbar />
-      <main className="w-full px-4 md:w-3/5 mx-auto min-h-full pb-20">
-        {children}
-      </main>
+      <main>{children}</main>
       <Footer />
     </div>
   )
