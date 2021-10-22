425eeec8f9b512b965f8f43200e206d18f40a28c
Mahciavelli <tmakaveli643@gmail.com>
2021-10-22 01:16:59 +0100

    setup minimum downloads for Tailwind and Babel

---
+++ snowpack.config.mjs
@@ -0,0 +1,26 @@
+export default {
+    env: {},
+    mount: {
+      public: '/',
+      src: '/dist',
+    },
+    packageOptions: {
+      types: true,
+    },
+    devOptions: {
+      port: 666,
+      tailwindConfig: './tailwind.config.js',
+    },
+    routes: [{ match: 'routes', src: '.*', dest: '/index.html' }],
+    plugins: [
+      '@snowpack/plugin-postcss',
+      '@snowpack/plugin-sass',
+      [
+        '@snowpack/plugin-babel',
+        {
+          input: ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
+        },
+      ],
+    ],
+  }
+  
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    mode: 'jit',
+    purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+    theme: {
+      extend: {
+        colors: {
+        },
+      },
+    },
+    variants: {
+    },
+  }
+  
